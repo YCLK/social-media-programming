be3ced2b1901bf9a71b3d8d9c17f364425710f57
YCLK(신유찬) <youchan8721@gmail.com>
2025-11-24 00:12:31 +0900

    Refactor print statements for model evaluation
    Updated print statements to clarify model evaluation results.

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-# pip install tensorflow==2.20.0
 from sklearn.metrics import r2_score, mean_squared_error
 from sklearn.linear_model import LinearRegression
 from sklearn.preprocessing import StandardScaler
@@ -24,18 +23,18 @@ print("회귀모델의 MSE :", mean_squared_error(y, lr.predict(X_scaled)))
 
 
 model = keras.models.load_model('my_model_R2_0.7892_MSE_0.4818.keras')
-
 y_pred = model.predict(X_scaled)
-r2 = r2_score(y, y_pred)
-mse = mean_squared_error(y, y_pred)
 
-print("결정계수:", r2)
-print("평균제곱오차:", mse)
+print("딥러닝 모델의 결정계수 :", r2_score(y, y_pred))
+print("딥러닝 모델의 MSE :", mean_squared_error(y, y_pred))
 
-print("[스크린타임 7시간 | 스트레스 7 | 수면의 질 2 → 행복도 예측]", (model.predict(np.array([[7, 7, 2]])))[0][0])
-print("[스크린타임 2시간 | 스트레스 2 | 수면의 질 7 → 행복도 예측]", (model.predict(np.array([[2, 2, 7]])))[0][0])
+print("[스크린타임 7시간 | 스트레스 7 | 수면의 질 2 → 행복도 예측]",
+      (model.predict(np.array([[7, 7, 2]])))[0][0])
+print("[스크린타임 2시간 | 스트레스 2 | 수면의 질 7 → 행복도 예측]",
+      (model.predict(np.array([[2, 2, 7]])))[0][0])
 
 residuals = y - y_pred.reshape(-1)
+
 
 plt.subplot(2, 1, 1)
 plt.scatter(lr.predict(X_scaled), y-lr.predict(X_scaled), marker='^', alpha=0.2)
